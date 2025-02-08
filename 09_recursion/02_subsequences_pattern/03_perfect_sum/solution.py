16e26ea8b03edac9bcd24a5948e3407ee71a34c0
Bishwaraj <deybishwaraj013@gmail.com>
2025-02-08 09:59:02 +0530

    perfect sum

---
+++ 09_recursion/02_subsequences_pattern/03_perfect_sum/solution.py
@@ -0,0 +1,25 @@
+# https://www.geeksforgeeks.org/problems/perfect-sum-problem5633/1
+class Solution:
+    def perfectSum(self, arr, target):
+        result = 0
+        curr_subset = []
+        
+        def dfs(index, curr_sum):
+            nonlocal result
+            if curr_sum == target:
+                result += 1
+                return
+            
+            if index >= len(arr) or curr_sum > target:
+                return
+            
+            # include current element
+            curr_subset.append(arr[index])
+            dfs(index+1, curr_sum + arr[index])
+            
+            # dont include curr element
+            curr_subset.pop()
+            dfs(index+1, curr_sum)
+        
+        dfs(0, 0)
+        return result
