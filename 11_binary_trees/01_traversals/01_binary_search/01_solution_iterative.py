f45c0550dd8dc42d5e010c6be7dcc6f27393b31a
Bishwaraj <deybishwaraj013@gmail.com>
2025-02-10 04:52:02 +0530

    binary search

---
+++ 11_binary_trees/01_traversals/01_binary_search/01_solution_iterative.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/binary-search/
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        low, high = 0, n-1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
