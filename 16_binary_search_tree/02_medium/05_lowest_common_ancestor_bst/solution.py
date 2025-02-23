04451432acf96539ecb18c0319e820143ec83338
Bishwaraj <deybishwaraj013@gmail.com>
2025-02-23 12:48:16 +0530

    LCA of BST

---
+++ 16_binary_search_tree/02_medium/05_lowest_common_ancestor_bst/solution.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+from typing import *
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+
+        while curr:
+            if p.val == curr.val or q.val == curr.val:
+                return curr
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                # it got split into different subtrees
+                return curr
+
+        return None
