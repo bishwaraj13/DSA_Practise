4ac62fae4f44e09dcb16b33d70aba659bf12bc40
Bishwaraj <deybishwaraj013@gmail.com>
2025-02-25 04:59:00 +0530

    triangle solution using memoization

---
+++ 10_dynamic_programming/2D/01_medium/05_triangle/02_solution_memoization.py
@@ -0,0 +1,24 @@
+# # https://leetcode.com/problems/triangle/
+from typing import *
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        memo = {}
+
+        def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            nonlocal n
+            if i == n - 1:
+                return triangle[i][j]
+
+            down = triangle[i][j] + dfs(i+1, j)
+            diagonal = triangle[i][j] + dfs(i+1, j+1)
+
+            memo[(i, j)] = min(down, diagonal)
+
+            return memo[(i, j)]
+
+        return dfs(0, 0)
+        
