064c4d77a517fc6a01a4e4ff105823022f28b1db
Bishwaraj <deybishwaraj013@gmail.com>
2025-02-27 15:23:06 +0530

    dir arrangement and floor/ceil problems

--- 11_binary_search/01_1D_arrays/01_easy/03_upper_bound/solution.py
+++ 11_binary_search/01_1D_arrays/01_easy/03_upper_bound/solution.py
@@ -1,11 +1,10 @@
-# https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1
 from typing import *
 
 class Solution:
     #User function Template for python3
     
     #Complete this function
-    def findFloor(self,arr,k):
+    def findCeil(self,arr,k):
         n = len(arr)
         ans = -1
         
@@ -18,9 +17,9 @@ class Solution:
             if arr[mid] == k:
                 return mid
             elif arr[mid] < k:
-                ans = mid
                 low = mid + 1
             elif arr[mid] > k:
+                ans = mid
                 high = mid - 1
                 
         return ans
