f91c2c2a917b2b5dee6628d4508ed9bceeb63235
Bishwaraj <deybishwaraj013@gmail.com>
2025-02-28 15:20:09 +0530

    finding sqrt using binary search

---
+++ 11_binary_search/02_answers/01_easy/01_finding_sqrt/solution.py
@@ -0,0 +1,17 @@
+# https://www.geeksforgeeks.org/problems/square-root/0
+class Solution:
+    def floorSqrt(self, n):
+        low = 1
+        high = n
+        ans = 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if (mid * mid) <= n:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return ans
