99876625792ed0e5722b3cd56de848cec393db64
Bishwaraj <deybishwaraj013@gmail.com>
2025-03-02 11:25:02 +0530

    find peal element ||

---
+++ 11_binary_search/03_2D_arrays/02_medium/03_find_peak_element_in _2d_matrix/solution.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/find-a-peak-element-ii/
+from typing import *
+
+class Solution:
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        ROWS = len(mat)
+        COLS = len(mat[0])
+
+        low = 0
+        high = COLS - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            maxRowIndex = self.findMaxIndex(mat, ROWS, COLS, mid)
+
+            left = -1
+            right = -1
+
+            if mid-1>=0:
+                left = mat[maxRowIndex][mid-1]
+
+            if (mid + 1) < COLS:
+                right = mat[maxRowIndex][mid+1]
+
+            if mat[maxRowIndex][mid] > left and mat[maxRowIndex][mid] > right:
+                return [maxRowIndex, mid]
+
+            elif (mat[maxRowIndex][mid] < left):
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return [-1, -1]
+
+    def findMaxIndex(self, mat, n, m, col):
+        maxValue = -1
+        index = -1
+
+        for i in range(n):
+            if mat[i][col] > maxValue:
+                maxValue = mat[i][col]
+                index = i
+            
+        return index
